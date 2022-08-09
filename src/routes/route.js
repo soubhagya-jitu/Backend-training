3bf81202becc90073cf1aa05abfde7cfbb2ed3d1
soubhagya-jitu <soubhagyasamal03@gmail.com>
2022-08-09 08:01:08 +0530

    assignment3

--- src/routes/route.js
+++ src/routes/route.js
@@ -33,6 +33,82 @@ router.get('/student-details/:name', function(req, res){
     console.log('Name of the student is ', studentName)
     
     res.send('Dummy response')
+
+
+
+
+})
+router.get('/get/movies',function(req,res) {
+    let film= ["iron man","captain america","thor","hulk"]
+    res.send(film)
 })
 
+
+
+router.get("/GET/movies/:indexNumber",function (req,res) {
+    let film= ["iron man","captain america","thor","hulk"]
+
+    let requestParams = req.params.indexNumber
+    let result5 = film[requestParams]
+    console.log (result5)
+    res.send(result5)
+})
+
+
+router.get('/get-moviess/:indexNumber',function(req, res){ //student detail api he 
+    let film= ["iron man","captain america","thor","hulk"]
+   
+    let index = req.params.indexNumber;
+
+     if(index >= film.length){
+        return res.send("use a valid index  ")
+     }else{
+    
+     res.send(film[index])
+     }
+})
+
+router.get("/get/films",function(req,res) {
+   let movies =  [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name:"Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+
+       res.send(movies)
+})
+
+router.get("/get/films/:filmId",function(req,res) {
+    let movies =  [ {
+         id: 1,
+         name: "The Shining"
+        }, {
+         id: 2,
+         name: "Incendies"
+        }, {
+         id: 3,
+         name:"Rang de Basanti"
+        }, {
+         id: 4,
+         name: "Finding Nemo"
+        }]
+
+        let index = req.params.filmId
+        if(index>=movies.length) {
+            res.send("No movie exists with this id")
+        } else {
+            res.send(movies[index])
+        }
+ })
+
+
 module.exports = router;
+
