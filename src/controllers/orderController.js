c5d9ae19c3d5cf96c7ab97970321776701b4cb7a
soubhagya-jitu <soubhagyasamal03@gmail.com>
2022-08-27 20:50:31 +0530

    ok

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,21 +1,37 @@
 const orderModel = require("../models/orderModel.js")
 const userModel = require("../models/userModel.js")
 const { find } = require("../models/userModel.js")
+const productModel = require("../models/productModel.js")
 
 
 
 const createOrder = async function(req,res) {
-    let a = req.body.userId
-    let b = req.body.productId
+    let userId = req.body.userId
+    let productId = req.body.productId
 
-    let result1 = await userModel.findById(a)
-    let result2 = await userModel.findById(b)
-    if (!result1 || !result2) {
+    let uniqueUser = await userModel.findById(userId)
+    let uniqueProduct = await productModel.findById(productId)
+    let freeuser = req.headers.isfreeappuser
+    if (!uniqueUser || !uniqueProduct) {
         res.send({msg:"either of the user or product not present"})
-    } else {
-        
     }
-    let order = await orderModel.create(req.body)
-    res.send({msg:order})
+    
+    else if(freeuser==="true") {
+        req.body["amount"] = 0
+        req.body["isFreeAppUser"] = true
+        Order = await orderModel.create(req.body)
+        res.send({msg:Order})
+    } else if (freeuser=="false") {
+        let productPrice = await productModel.findById(productId).select({price:1,_id:0})
+        let userPrice = await userModel.findById(userId).select({balance:1,_id:0})
+        let finalPrice = userPrice.balance-productPrice.price
+        let updateUserPrice = await userModel.findByIdAndUpdate(userId,{balance:finalPrice})
+        req.body["amount"] = productPrice.price
+        req.body["isFreeAppUser"] = false
+        Order = await orderModel.create(req.body)
+        res.send({msg:Order})
+
+    }
+    
 }
 module.exports.createOrder = createOrder
